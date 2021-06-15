4ec2b40534c4880c75ffd182669a0c67a3160567
siddh <siddh@DESKTOP-2I1D84J>
2021-06-15 10:38:43 +0530

    Add project files.

---
+++ ToDoList/Infrastructure/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Infrastructure
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        { }
+        public DbSet<TodoList> TodoList { get; set; }
+    }
+}
